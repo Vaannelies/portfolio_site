9cdc156520c8aa046325313ac4bd1ff5817d4df7
Annelies <43701941+Vaannelies@users.noreply.github.com>
2018-12-01 13:40:21 +0100

    Ik heb een PHP bestand van het HTML bestand gemaakt.
    Nu is het 'prompt venster' interactief.

---
+++ portfoliokopie.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html>
+
+
+
+<head>
+<link rel="stylesheet" href="portfolio.css">
+
+</head>
+
+
+<body>
+  
+  <?php
+  $bericht = "";
+if(isset($_POST["onderwerp"])){
+	
+
+
+$gegeven = $_POST["onderwerp"];
+$bericht = "hoi";
+$berichttwee = "Je hebt dit ingevoerd: ".$gegeven;
+
+
+if($gegeven == "achtergrond"){
+$bericht = ">>> Hond";
+
+} elseif ($gegeven == "hobbys"){
+$bericht = ">>> Kat";
+}
+else{
+$bericht = ">>> Niks";
+}
+}
+?>
+
+  <header> <!-- hier vind je de titel enzo-->
+    <div class="header"><span>
+      
+    
+      <h1>  Annelies </h1> </span>
+  <h2>  - Een enthousiaste student aan de Hogeschool Rotterdam - </h2>
+     
+    </div>
+  </header> <!-- tot hier-->
+
+  <!-- verdere inhoud van de pagina -->
+  <div class="main">
+<br>
+   
+		  <div class="gegevens">
+			
+		 <span class="gegevens">
+		
+		  <br>
+		
+		   >> NAAM: Annelies <br>
+		   >> GEBOORTEDATUM: 10 juli 2000 <br>
+		  >> OPLEIDING: Creative Media and Game Technologies <br>
+		  >> LEERJAAR: 1 <br><br>
+		   
+			<form method="post" action="">
+			
+		   >> WAAR WIL JE MEER OVER WETEN? <br>
+		  >> TYPE achtergrond OF hobby's
+			<input type="text" name="onderwerp"> 
+			<button type = "submit"> Verzenden </button>
+		  </form
+		    <?php echo "$bericht";?>
+		  
+		   </span>
+		   		  <!-- hieronder zie je een stukje php dat test of er een post is gegeven  -->
+
+		
+			
+		  </div>
+
+	</div>
+ 
+  
+ <script src="myscripts.js"></script> 
+</body>
+
+
+
+<footer>
+Test test
+</footer>
+
+
+
+</html>
